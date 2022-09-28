3c6d09450c30c73b25a5e3c77c33eea670a9942e
MaichelRazkalla <83008684+MaichelRazkalla@users.noreply.github.com>
2022-09-28 11:55:44 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,21 @@
-sudo su
+sudo true
 # Alternatively you can use the official docker install script
 wget -qO- https://get.docker.com/ | sh
 # Install docker-compose
-DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
-mkdir -p $DOCKER_CONFIG/cli-plugins
-curl -SL https://github.com/docker/compose/releases/download/v2.11.2/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
-chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
-chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
-docker compose version
+COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oE "[0-9]+\.[0-9][0-9]+\.[0-9]+$" | sort --version-sort | tail -n 1`
+sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+sudo chmod +x /usr/local/bin/docker-compose
+sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
+# Install docker-cleanup command
+cd /tmp
+git clone https://gist.github.com/76b450a0c986e576e98b.git
+cd 76b450a0c986e576e98b
+sudo mv docker-cleanup /usr/local/bin/docker-cleanup
+sudo chmod +x /usr/local/bin/docker-cleanup
+# Install Tuba/
+mkdir ~/Tuba/
+cd ~/Tuba/ 
+git clone git@gitlab.com:devisionx/ai-tool/tuba-frontend.git
+cd tuba-frontend
+docker build -t tuba .
+docker run -it --rm -d -p 3001:3000 --name tuba tuba
