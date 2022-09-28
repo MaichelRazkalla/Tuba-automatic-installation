c9582c809b583b894289eb8522d1ccdad2545072
MaichelRazkalla <83008684+MaichelRazkalla@users.noreply.github.com>
2022-09-28 04:11:38 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,25 +1,10 @@
-
+sudo su
 # Alternatively you can use the official docker install script
 wget -qO- https://get.docker.com/ | sh
-
 # Install docker-compose
-
 DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
 mkdir -p $DOCKER_CONFIG/cli-plugins
 curl -SL https://github.com/docker/compose/releases/download/v2.11.2/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
- 
 chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
- 
-sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
-
+chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
 docker compose version
-
- 
- 
-
-
-
-
-
- 
- 
